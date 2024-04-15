6b9d44867b88bc1623782849ce16e629e6947b94
V.Rajadurai <148923104+Rajadurai248@users.noreply.github.com>
2024-04-15 20:35:32 +0530

    Add files via upload
    upload

---
+++ Day 5/resume/js/resume.js
@@ -0,0 +1,54 @@
+var resumeData = [{
+    "name": "Rajadurai",
+    "email": "vrajadurai.248@gmail.com",
+    "phone": "+91 7825065187",
+    "address": "2/200 ilangudi pavanakottai devakottai",
+    "summary": "i am current MCA student i learned html and css. and i am studying full stack MERN stack in guvi now",
+    "education": [
+      {
+        "degree": "Bachelor of Science in Computer Science , Master of computer application",
+        "institution": "Ananda college , JJ college of arts an dscience",
+        "location": "devakottai, Tamilnadu, pudukottai , Tamilnadu",
+        "graduation_year": "2022,2024"
+      }
+    ],
+    "experience": [
+      {
+        "title": "i make one project ",
+        "company": "Free lancing",
+        "location": "Devakottai, Tamilnadu",
+        "start_date": "2022",
+        "end_date": "2023",
+        "responsibilities": [
+          "Developed scalable web applications using React.js and Node.js",
+          "Collaborated with cross-functional teams to deliver projects on time",
+          "Led code reviews and mentored junior developers"
+        ]
+      },
+      {
+        "title": "web developer",
+        "company": "free lancing",
+        "location": "devakottai, Tamilnadu",
+        "start_date": "2022",
+        "end_date": "2023",
+        "responsibilities": [
+          "Designed and implemented RESTful APIs for mobile applications",
+          "Contributed to the architecture of a microservices-based platform",
+          "Participated in agile ceremonies and sprint planning"
+        ]
+      }
+    ],
+    "skills": [
+      "JavaScript",
+      "React.js",
+      "Node.js",
+      "HTML",
+      "CSS",
+      "Git",
+      "Github"
+    ]
+  
+  
+  }];
+  
+  console.log("resumeData",resumeData);
